4f0dd7cc6911eda0f15df3fbb1ec3b79f1a56a88
Andras Novoszath <nocibambi@gmail.com>
2024-11-08 05:41:41 +0100

    Temp archive unused modules

--- archive/calculate_daily_balance.py
+++ archive/calculate_daily_balance.py
@@ -4,14 +4,16 @@ previous_timestamp = 1727551654
 timestamp = 1727551655
 period_in_days = timestamp - previous_timestamp
 
+
 def total_borrows(timestamp):
     return total_stable_borrows(timestamp) + total_variable_borrows(timestamp)
+
 
 def borrow_rate(timestamp):
     return (
         variable_borrow_rate(timestamp) * total_variable_borrows(timestamp)
         + stable_borrow_rate(timestamp) * total_stable_borrows(timestamp)
-    ) / total_borrows(timestamp))
+    ) / total_borrows(timestamp)
 
 
 def utilization_rate(timestamp):
