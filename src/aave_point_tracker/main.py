c20abcc829c20ef0937d94c66d8e3525eba20dd5
Andras Novoszath <nocibambi@gmail.com>
2024-11-13 08:19:27 +0100

    main function

---
+++ src/aave_point_tracker/main.py
@@ -0,0 +1,33 @@
+from aave_point_tracker.data_collection.atoken_balance_histories import (
+    fetch_atoken_balance_histories,
+)
+from aave_point_tracker.data_collection.reserve_assets import fetch_reserve_assets
+from aave_point_tracker.data_collection.starting_balances import fetch_starting_balances
+from aave_point_tracker.data_collection.aave_addresses import fetch_aave_addresses
+from aave_point_tracker.data_collection.reserve_liquidity_indexes import (
+    fetch_reserve_liquidity_indexes,
+)
+from aave_point_tracker.data_collection.reserve_asset_prices import (
+    fetch_reserve_asset_prices,
+)
+
+from aave_point_tracker.calculation.data_preparation import prepare_datasets
+
+from aave_point_tracker.calculation.points_calculation import calculate_user_tvls
+
+
+def main():
+    fetch_aave_addresses()
+    fetch_atoken_balance_histories()
+    fetch_reserve_asset_prices()
+    fetch_reserve_assets()
+    fetch_reserve_liquidity_indexes()
+    fetch_starting_balances()
+
+    prepare_datasets()
+
+    calculate_user_tvls()
+
+
+if __name__ == "__main__":
+    main()
